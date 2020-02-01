dac8a73d179fd4853fb0908dba93bb0b60a3b6fa
Brennon Nuckols <brennon@Brennons-MacBook-Pro.local>
2020-01-31 18:04:48 -0700

    Step 1

--- sprintchallenge/src/Components/Form.js
+++ sprintchallenge/src/Components/Form.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
-const Form = () => {
-
+const TheForm = ({values}) => {
+    return (
+        <div>
+            <Form>
+                <label htmlFor="name">
+                    Name:
+                </label>
+                <Field
+                    type = "text"
+                    name = "name"
+                    id = "name"
+                />
+                <label htmlFor="email">
+                    Email
+                </label>
+                <Field
+                    type = "text"
+                    name = "email"
+                    id = "email"
+                />
+                <label htmlFor="password">
+                    Password
+                </label>
+                <Field
+                    type = "text"
+                    name = "password"
+                    id = "password"
+                />
+                <label className = "checkmark">
+                    Terms of Service
+                </label>
+                <Field
+                    type = "checkbox"
+                    name = "TermsOfService"
+                    checked = {values.termsOfService}
+                />
+                <span className = "checkmark" />
+                <button type="submit">Submit</button>
+            </Form>
+        </div>
+    )
 }
 
-export default Form;
+export default TheForm;
